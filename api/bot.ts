892b5791c5e7516331147c070a709908412b4f8b
Adam Zhu <lei.zhu@live.cn>
2023-07-22 11:48:45 +0800

    menu test

--- api/bot.ts
+++ api/bot.ts
@@ -1,14 +1,10 @@
-// import { Bot, webhookCallback } from "grammy";
-// import { Menu } from "@grammyjs/menu";
+import { Bot, Context, session, SessionFlavor, webhookCallback } from "grammy";
+import { Menu, MenuRange } from "@grammyjs/menu";
 
-// const token = process.env.BOT_TOKEN;
-// if (!token) throw new Error("BOT_TOKEN is unset");
+const token = process.env.BOT_TOKEN;
+if (!token) throw new Error("BOT_TOKEN is unset");
 
-// // create a bot
-// const bot = new Bot(token);
-
-// // create a menu
-
+// create a menu
 // const switches = new Set<number>();
 
 // function toggleSwitches(id: number) {
@@ -35,127 +31,69 @@
 //     },
 //   ).row();
 
-// // apply menu
+// apply menu
 // bot.use(menu);
 
-// bot.command("start", async (ctx) => {
-//   // send menu
-//   await ctx.reply("This is my first telegram bot:", { reply_markup: menu });
-// });
+enum MenuType {
+  Buy = 1,
+  Sell = 2,
+}
 
-// export default webhookCallback(bot, 'http')
-
-import { Bot, Context, session, SessionFlavor, webhookCallback } from 'grammy'
-import { Menu, MenuRange } from '@grammyjs/menu'
-
-/** This is how the dishes look that this bot is managing */
-interface Dish {
-  id: string
-  name: string
+interface MenuItem {
+  id: string,
+  text: string,
+  type: MenuType,
 }
 
 interface SessionData {
-  favoriteIds: string[]
+  isSell: Boolean,
 }
 
 type MyContext = Context & SessionFlavor<SessionData>
 
-const dishDatabase: Dish[] = [
-  { id: 'pasta', name: 'Pasta' },
-  { id: 'pizza', name: 'Pizza' },
-  { id: 'sushi', name: 'Sushi' },
-  { id: 'entrct', name: 'Entrecôte' },
+const dynamicMenu: MenuItem[] = [
+  { id: 'buy001ETH', text: 'Buy 0.01 ETH', type: MenuType.Buy },
+  { id: 'buy005ETH', text: 'Buy 0.05 ETH', type: MenuType.Buy },
+  { id: 'sell001ETH', text: 'Sell 0.01 ETH', type: MenuType.Sell },
+  { id: 'sell005ETH', text: 'Sell 0.05 ETH', type: MenuType.Sell },
 ]
 
-const token = process.env.BOT_TOKEN;
-if (!token) throw new Error("BOT_TOKEN is unset");
-const bot = new Bot<MyContext>(token)
+// create a bot
+const bot = new Bot<MyContext>(token);
 
 bot.use(
   session({
     initial(): SessionData {
-      return { favoriteIds: [] }
+      return { isSell: false }
     },
   })
 )
 
-// Create a dynamic menu that lists all dishes in the dishDatabase,
-// one button each
-const mainText = 'Pick a dish to rate it!'
-const foodMenu = new Menu<MyContext>('food-menu')
-foodMenu.dynamic(() => {
-  const range = new MenuRange<MyContext>()
-  for (const dish of dishDatabase) {
-    range
-      .submenu(
-        { text: dish.name, payload: dish.id }, // label and payload
-        'dish-menu', // navigation target menu
-        ctx =>
-          ctx.editMessageText(dishText(dish.name), {
-            parse_mode: 'HTML',
-          }) // handler
-      )
-      .row()
-  }
-  return range
-})
-
-// Create the sub-menu that is used for rendering dishes
-const dishText = (dish: string) => `<b>${dish}</b>\n\nYour rating:`
-const dishMenu = new Menu<MyContext>('dish-menu')
-dishMenu.dynamic(ctx => {
-  const dish = ctx.match
-  if (typeof dish !== 'string') throw new Error('No dish chosen!')
-  return createDishMenu(dish)
-})
-
-/** Creates a menu that can render any given dish */
-function createDishMenu(dish: string) {
-  return new MenuRange<MyContext>()
-    .text(
-      {
-        text: ctx => ctx.session.favoriteIds.includes(dish) ? 'Yummy!' : 'Meh.',
-        payload: dish,
-      },
-      ctx => {
-        const set = new Set(ctx.session.favoriteIds)
-        if (!set.delete(dish)) set.add(dish)
-        ctx.session.favoriteIds = Array.from(set.values())
-        ctx.menu.update()
+const testMenu = new Menu<MyContext>('test-menu');
+testMenu
+  .text("Add", (ctx) => ctx.reply("You pressed Add!"))
+  .text("Switch", (ctx) => {
+    ctx.session.isSell = !ctx.session.isSell;
+    ctx.menu.update();
+  }).row()
+  .dynamic((ctx) => {
+    const range = new MenuRange<MyContext>();
+    if (ctx.session.isSell) {
+      for (const menu of dynamicMenu.filter(menu => menu.type === MenuType.Sell)) {
+        range.text(menu.text.toString(), (ctx) => ctx.reply(`You pressed ${menu.text}.`));
       }
-    )
-    .row()
-    .back({ text: 'X Delete', payload: dish }, async ctx => {
-      const index = dishDatabase.findIndex(d => d.id === dish)
-      dishDatabase.splice(index, 1)
-      await ctx.editMessageText('Pick a dish to rate it!')
-    })
-    .row()
-    .back({ text: 'Back', payload: dish })
-}
+    } else {
+      for (const menu of dynamicMenu.filter(menu => menu.type === MenuType.Buy)) {
+        range.text(menu.text.toString(), (ctx) => ctx.reply(`You pressed ${menu.text}.`));
+      }
+    }
+    return range;
+  });
 
-foodMenu.register(dishMenu)
-bot.use(foodMenu)
-
-bot.command('start', ctx => ctx.reply(mainText, { reply_markup: foodMenu }))
-bot.command('help', async ctx => {
-  const text = 'Send /start to see and rate dishes. Send /fav to list your favorites!'
-  await ctx.reply(text)
-})
-bot.command('fav', async ctx => {
-  const favs = ctx.session.favoriteIds
-  if (favs.length === 0) {
-    await ctx.reply('You do not have any favorites yet!')
-    return
-  }
-  const names = favs
-    .map(id => dishDatabase.find(dish => dish.id === id))
-    .filter((dish): dish is Dish => dish !== undefined)
-    .map(dish => dish.name)
-    .join('\n')
-  await ctx.reply(`Those are your favorite dishes:\n\n${names}`)
-})
-
-bot.catch(console.error.bind(console))
+bot.use(testMenu);
+bot.command("start", async (ctx) => {
+  // send menu
+  await ctx.reply("This is my first telegram bot:", { reply_markup: testMenu });
+});
 
 export default webhookCallback(bot, 'http')
---
+++ api/example.ts
@@ -0,0 +1,115 @@
+
+import { Bot, Context, session, SessionFlavor, webhookCallback } from 'grammy'
+import { Menu, MenuRange } from '@grammyjs/menu'
+
+/** This is how the dishes look that this bot is managing */
+interface Dish {
+  id: string
+  name: string
+}
+
+interface SessionData {
+  favoriteIds: string[]
+}
+
+type MyContext = Context & SessionFlavor<SessionData>
+
+const dishDatabase: Dish[] = [
+  { id: 'pasta', name: 'Pasta' },
+  { id: 'pizza', name: 'Pizza' },
+  { id: 'sushi', name: 'Sushi' },
+  { id: 'entrct', name: 'Entrecôte' },
+]
+
+const token = process.env.BOT_TOKEN;
+if (!token) throw new Error("BOT_TOKEN is unset");
+const bot = new Bot<MyContext>(token)
+
+bot.use(
+  session({
+    initial(): SessionData {
+      return { favoriteIds: [] }
+    },
+  })
+)
+
+// Create a dynamic menu that lists all dishes in the dishDatabase,
+// one button each
+const mainText = 'Pick a dish to rate it!'
+const foodMenu = new Menu<MyContext>('food-menu')
+foodMenu.dynamic(() => {
+  const range = new MenuRange<MyContext>()
+  for (const dish of dishDatabase) {
+    range
+      .submenu(
+        { text: dish.name, payload: dish.id }, // label and payload
+        'dish-menu', // navigation target menu
+        ctx =>
+          ctx.editMessageText(dishText(dish.name), {
+            parse_mode: 'HTML',
+          }) // handler
+      )
+      .row()
+  }
+  return range
+})
+
+// Create the sub-menu that is used for rendering dishes
+const dishText = (dish: string) => `<b>${dish}</b>\n\nYour rating:`
+const dishMenu = new Menu<MyContext>('dish-menu')
+dishMenu.dynamic(ctx => {
+  const dish = ctx.match
+  if (typeof dish !== 'string') throw new Error('No dish chosen!')
+  return createDishMenu(dish)
+})
+
+/** Creates a menu that can render any given dish */
+function createDishMenu(dish: string) {
+  return new MenuRange<MyContext>()
+    .text(
+      {
+        text: ctx => ctx.session.favoriteIds.includes(dish) ? 'Yummy!' : 'Meh.',
+        payload: dish,
+      },
+      ctx => {
+        const set = new Set(ctx.session.favoriteIds)
+        if (!set.delete(dish)) set.add(dish)
+        ctx.session.favoriteIds = Array.from(set.values())
+        ctx.menu.update()
+      }
+    )
+    .row()
+    .back({ text: 'X Delete', payload: dish }, async ctx => {
+      const index = dishDatabase.findIndex(d => d.id === dish)
+      dishDatabase.splice(index, 1)
+      await ctx.editMessageText('Pick a dish to rate it!')
+    })
+    .row()
+    .back({ text: 'Back', payload: dish })
+}
+
+foodMenu.register(dishMenu)
+bot.use(foodMenu)
+
+bot.command('start', ctx => ctx.reply(mainText, { reply_markup: foodMenu }))
+bot.command('help', async ctx => {
+  const text = 'Send /start to see and rate dishes. Send /fav to list your favorites!'
+  await ctx.reply(text)
+})
+bot.command('fav', async ctx => {
+  const favs = ctx.session.favoriteIds
+  if (favs.length === 0) {
+    await ctx.reply('You do not have any favorites yet!')
+    return
+  }
+  const names = favs
+    .map(id => dishDatabase.find(dish => dish.id === id))
+    .filter((dish): dish is Dish => dish !== undefined)
+    .map(dish => dish.name)
+    .join('\n')
+  await ctx.reply(`Those are your favorite dishes:\n\n${names}`)
+})
+
+bot.catch(console.error.bind(console))
+
+export default webhookCallback(bot, 'http')
