e03b61ef1cec779f1b11cfe27d8b163186be17ac
Adam Zhu <lei.zhu@live.cn>
2023-07-22 10:43:39 +0800

    Update bot.ts

--- api/bot.ts
+++ api/bot.ts
@@ -8,10 +8,24 @@ if (!token) throw new Error("BOT_TOKEN is unset");
 const bot = new Bot(token);
 
 // create a menu
+
+const switches = new Set<number>();
+
+function toggleSwitches(id: number) {
+  if (switches.has(id)) switches.delete(id);
+  else switches.add(id);
+}
+
 const menu = new Menu("my-menu-identifier")
   .text("Add", (ctx) => ctx.reply("You pressed Add!"))
-  .text("Switch", (ctx) => ctx.reply("You pressed Switch!")).row()
-  .text("Buy 0.01", (ctx) => ctx.reply("You pressed B 0.01!"))
+  .text("Switch", (ctx) => {
+    toggleSwitches(ctx.from.id);
+    ctx.menu.update();
+  }).row()
+  .text(
+    (ctx) => `${ctx.from && switches.has(ctx.from.id) ? "Sell 0.01" : "Buy 0.01"}`, // 动态标签
+    (ctx) => ctx.reply(`Hello ${ctx.from.first_name}!`),
+  )
   .text("Buy 0.05", (ctx) => ctx.reply("You pressed B 0.05!")).row();
 
 // apply menu
