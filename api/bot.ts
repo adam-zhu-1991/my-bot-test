c6f48e79c20fb5f1cca7f0edefa8fc5210cd65a4
Adam Zhu <lei.zhu@live.cn>
2023-07-22 14:21:57 +0800

    Update bot.ts

--- api/bot.ts
+++ api/bot.ts
@@ -122,7 +122,7 @@ async function setWalletAddress(conversation: MyConversation, ctx: MyContext) {
 }
 
 async function createWalletSuccess(conversation: MyConversation, ctx: MyContext) {
-  const message = `✅Added ARB Wallet(💳${ctx.session.walletName})${ctx.session.walletAddress}`;
+  const message = `✅Added ARB Wallet(💳${ctx.session.walletName})\n${ctx.session.walletAddress}`;
   await ctx.reply(message, {
     parse_mode: "HTML"
   });
